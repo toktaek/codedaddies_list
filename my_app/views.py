5967d7d4d7075fe73d54cd28f2bfc0a49e7ddc27
chaleelaleela <chaleelaleela@gmail.com>
2021-03-03 15:14:32 +0700

    Adiing b4s functionality.
    à

--- my_app/views.py
+++ my_app/views.py
@@ -1,15 +1,60 @@
+import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
-import requests
+from . import  models
 # Create your views here.
+
+BASE_CRAIGSLIST_URL = 'https://bangkok.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+
 def home(request):
     return render(request, template_name='base.html')
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+
+    data = response.text
+
+    soup = BeautifulSoup(data, features='html.parser')
+
+    #post_titles = soup.find_all('a', {'class': 'result-title'}) #what does 'a' mean
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+    #post_title = post_listings[0].find(class_ = 'result-title').text
+    #post_url = post_listings[0].find('a').get('href')
+    #post_price = post_listings[0].find(class_ = 'result-price').text
+
+    final_postings = []
+    for post in post_listings:
+        post_title = post.find(class_ = 'result-title').text
+        post_url   = post.find('a').get('href')
+        post_price = 'N/A'
+        post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+
+        if post.find(class_ = 'result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_ = 'result-image').get('data-ids'):  # we want to get just 1 image.
+            post_image_id = post.find(class_ = 'result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'  #no imgae
+
+
+        final_postings.append((post_title, post_url, post_price, post_image_url))
+
+
     stuff_for_frontend = {
         'search' : search,
-        'bibi' : 'bibixx'
+        'final_postings' : final_postings,
     }
+
     return render(request,  'my_app/new_search.html', stuff_for_frontend)
